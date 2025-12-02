ac810c4c7678d8a497b173cc7c4e3b5ae29ff0e4
Tanawat Srisukphrom <121351603+AiMlxV@users.noreply.github.com>
2025-12-02 23:01:32 +0700

    Improve DFS for cycle detection in graph
    Fix cycle detection in DFS by adding cycle size output.

--- Nov-26__Graph/a59_q3_cycle.cpp
+++ Nov-26__Graph/a59_q3_cycle.cpp
@@ -4,9 +4,7 @@
 #include <iostream>
 #include <variant>
 #include <vector>
-
 using namespace std;
-
 vector<int> adj[100005];
 int depth[100005];
 bool visited[100005];
@@ -14,19 +12,17 @@ bool visited[100005];
 void dfs(int u, int parent, int d){
     visited[u] = true;
     depth[u] = d;
-
     for (auto &v : adj[u]) {
         if (v == parent) {
             continue;
         }
-
+        
         if (!visited[v]) {
             dfs(v, u, d+1);
         }
         else{
             int cycle_size = abs(depth[u] - depth[v]) + 1;
             cout << cycle_size << endl;
-
             exit(0);
         }
     }
